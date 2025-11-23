e430ffa7b3b43d4f96a5dcdf9c5bdf67f2370a8e
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2025-11-23 22:08:46 +0000

    feat: redirect when user authenticated

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 const protectedRoutes: Route[] = ['/home'];
+const authRoute: Route = '/auth';
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
@@ -17,14 +18,28 @@ const protectedRouteProxy = async (req: NextRequest) => {
   return NextResponse.next();
 };
 
+const authRouteProxy = async (req: NextRequest) => {
+  const cookieStore = await cookies();
+  const jwt = cookieStore.get('jwt');
+  if (jwt) {
+    return NextResponse.redirect(new URL('/home', req.nextUrl));
+  }
+  return NextResponse.next();
+};
+
 const proxy = async (req: NextRequest) => {
   const path = req.nextUrl.pathname;
+
   const isProtectedRoute = protectedRoutes.some((route) =>
     path.startsWith(route),
   );
-
   if (isProtectedRoute) {
     return protectedRouteProxy(req);
+  }
+
+  const isAuthRoute = path.startsWith(authRoute);
+  if (isAuthRoute) {
+    return authRouteProxy(req);
   }
 
   return NextResponse.next();
